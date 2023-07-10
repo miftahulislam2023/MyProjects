d6d84b9d7a7ddec16300465b3e830d945aa37c61
Miftahul Islam <138649177+miftahulislam2023@users.noreply.github.com>
2023-07-10 17:00:59 +0600

    Initial commit

---
+++ tasbeeh-counter/index.js
@@ -0,0 +1,12 @@
+let count = 0
+
+function subhanallah() {
+    count++
+document.getElementById("counter-el").innerText = count
+}
+
+function resetCounter() {
+    count = 0
+document.getElementById("counter-el").innerText = count
+}
+
