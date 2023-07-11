951decacfbd6c4de98e30589a96d472a6938bedb
Miftahul Islam <138649177+miftahulislam2023@users.noreply.github.com>
2023-07-11 09:50:35 +0600

    Update index.js

--- tasbeeh-counter/index.js
+++ tasbeeh-counter/index.js
@@ -1,12 +1,22 @@
 let count = 0
+let counts = new Array()
+let history = ""
 
 function subhanallah() {
     count++
-document.getElementById("counter-el").innerText = count
+    document.getElementById("counter-el").innerText = count
 }
 
 function resetCounter() {
     count = 0
-document.getElementById("counter-el").innerText = count
+    document.getElementById("counter-el").innerText = count
+}
+
+function saveHistory() {
+   counts[count] = count
+   history = history + counts[count] + " - "
+   count = 0
+    document.getElementById("count-history").innerText = history
+    document.getElementById("counter-el").innerText = count
 }
 
